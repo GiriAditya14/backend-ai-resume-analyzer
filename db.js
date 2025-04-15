20d3399475fc2fcf27b4725c9fc21552949db2d2
GiriAditya14 <aditya.2004.giri@gmail.com>
2025-04-16 05:05:46 +0530

    updated db.js

--- db.js
+++ db.js
@@ -2,11 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/resume-analyzer', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
